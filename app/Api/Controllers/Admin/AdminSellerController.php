cda479dd1167d46c5a14ae80044bac13fa7f6bed
Dimas Pondra Oktafianto <77646429+DimasPondra@users.noreply.github.com>
2023-07-06 11:47:44 +0700

    :sparkles: add CRUD sellers

---
+++ app/Api/Controllers/Admin/AdminSellerController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Api\Controllers\Admin;
+
+use App\Api\Requests\SellerStoreRequest;
+use App\Api\Requests\SellerUpdateRequest;
+use App\Api\Resources\SellerResourceCollection;
+use App\Http\Controllers\Controller;
+use App\Models\Seller;
+use App\Repositories\SellerRepository;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class AdminSellerController extends Controller
+{
+    private $sellerRepository;
+
+    public function __construct(SellerRepository $sellerRepository)
+    {
+        $this->sellerRepository = $sellerRepository;
+    }
+
+    public function index(Request $request)
+    {
+        $sellers = $this->sellerRepository->get([
+            'search' => [
+                'name' => $request->name
+            ],
+            'paginate' => $request->per_page
+        ]);
+
+        return new SellerResourceCollection($sellers);
+    }
+
+    public function store(SellerStoreRequest $request)
+    {
+        try {
+            DB::beginTransaction();
+
+            $request->merge(['slug' => Str::slug($request->name)]);
+
+            $data = $request->only(['name', 'slug']);
+
+            $seller = new Seller();
+            $this->sellerRepository->save($seller->fill($data));
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            return response()->json([
+                'message' => 'Something went wrong, ' . $th->getMessage()
+            ], 500);
+        }
+
+        return response()->json([
+            'message' => 'Seller successfully created.'
+        ], 201);
+    }
+
+    public function update(SellerUpdateRequest $request, Seller $seller)
+    {
+        try {
+            DB::beginTransaction();
+
+            $request->merge(['slug' => Str::slug($request->name)]);
+
+            $data = $request->only(['name', 'slug']);
+
+            $this->sellerRepository->save($seller->fill($data));
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            return response()->json([
+                'message' => 'Something went wrong, ' . $th->getMessage()
+            ], 500);
+        }
+
+        return response()->json([
+            'message' => 'Seller successfully updated.'
+        ], 201);
+    }
+
+    public function destroy(Seller $seller)
+    {
+        try {
+            DB::beginTransaction();
+
+            $seller->delete();
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            return response()->json([
+                'message' => 'Something went wrong, ' . $th->getMessage()
+            ], 500);
+        }
+
+        return response()->json([
+            'message' => 'Seller successfully deleted.'
+        ], 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Api\Controllers\Admin\AdminProductController;
 use App\Api\Controllers\Admin\AdminProductTypeController;
 use App\Api\Controllers\Admin\AdminUserController;
 use App\Api\Controllers\Admin\AdminRoleController;
+use App\Api\Controllers\Admin\AdminSellerController;
 use App\Api\Controllers\AuthController;
 use App\Api\Controllers\Cashier\CashierCartController;
 use App\Api\Controllers\Cashier\CashierTransactionController;
@@ -54,6 +55,13 @@ Route::prefix('admin')->middleware('auth:sanctum', 'abilities:admin')->group(fun
 
     Route::prefix('files')->group(function () {
         Route::post('store', [AdminFileController::class, 'store']);
+    });
+
+    Route::prefix('sellers')->group(function () {
+        Route::get('/', [AdminSellerController::class, 'index']);
+        Route::post('store', [AdminSellerController::class, 'store']);
+        Route::patch('{seller}/update', [AdminSellerController::class, 'update']);
+        Route::delete('{seller}/delete', [AdminSellerController::class, 'destroy']);
     });
 });
 

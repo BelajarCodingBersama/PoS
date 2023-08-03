550881790046783748668b17a9b409ddb77df6d0
Dimas Pondra Oktafianto <77646429+DimasPondra@users.noreply.github.com>
2023-08-03 12:08:48 +0700

    tidy up writing

--- app/Api/Controllers/Admin/AdminRoleController.php
+++ app/Api/Controllers/Admin/AdminRoleController.php
@@ -15,15 +15,15 @@ use Illuminate\Support\Str;
 
 class AdminRoleController extends Controller
 {
-   private $roleRepository;
+    private $roleRepository;
 
-   public function __construct(RoleRepository $roleRepository)
-   {
+    public function __construct(RoleRepository $roleRepository)
+    {
         $this->roleRepository = $roleRepository;
-   }
+    }
 
-   public function index(Request $request)
-   {
+    public function index(Request $request)
+    {
         $roles = $this->roleRepository->get([
             'search' => [
                 'name' => $request->name
@@ -32,97 +32,97 @@ class AdminRoleController extends Controller
         ]);
 
         return new RoleResourceCollection($roles);
-   }
+    }
 
-   public function store(RoleStoreRequest $request, Role $role)
-   {
-     try {
-          DB::beginTransaction();
+    public function store(RoleStoreRequest $request, Role $role)
+    {
+        try {
+            DB::beginTransaction();
 
-          $request->merge([
-               'slug' => Str::slug($request->name)
-          ]);
+            $request->merge([
+                'slug' => Str::slug($request->name)
+            ]);
 
-          $data = $request->only(['name', 'slug']);
+            $data = $request->only(['name', 'slug']);
 
-          $role = new Role();
-          $this->roleRepository->save($role->fill($data));
+            $role = new Role();
+            $this->roleRepository->save($role->fill($data));
 
-          DB::commit();
-     } catch (\Throwable $th) {
-          DB::rollBack();
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
 
-          return response()->json([
-               'message' => 'Something went wrong, ' . $th->getMessage()
-          ], 500);
-     }
+            return response()->json([
+                'message' => 'Something went wrong, ' . $th->getMessage()
+            ], 500);
+        }
 
-     return response()->json([
-          'message' => 'Role successfully created.'
-     ], 201);
-   }
+        return response()->json([
+            'message' => 'Role successfully created.'
+        ], 201);
+    }
 
     public function show(Role $role)
     {
         return new RoleResource($role);
     }
 
-   public function update(RoleUpdateRequest $request, Role $role)
-   {
-     try {
-          DB::beginTransaction();
+    public function update(RoleUpdateRequest $request, Role $role)
+    {
+        try {
+            DB::beginTransaction();
 
-          $request->merge([
-               'slug' => Str::slug($request->name)
-          ]);
+            $request->merge([
+                'slug' => Str::slug($request->name)
+            ]);
 
-          $data = $request->only(['name', 'slug']);
+            $data = $request->only(['name', 'slug']);
 
-          $this->roleRepository->save($role->fill($data));
+            $this->roleRepository->save($role->fill($data));
 
-          DB::commit();
-     } catch (\Throwable $th) {
-          DB::rollback();
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollback();
 
-          return response()->json([
-               'message' => 'Something went wrong, ' . $th->getMessage()
-          ], 500);
-     }
+            return response()->json([
+                'message' => 'Something went wrong, ' . $th->getMessage()
+            ], 500);
+        }
 
-     return response()->json([
-          'message' => 'Role successfully updated.'
-     ], 201);
-   }
+        return response()->json([
+            'message' => 'Role successfully updated.'
+        ], 201);
+    }
 
-   public function destroy(Role $role)
-   {
-     try {
-          DB::beginTransaction();
+    public function destroy(Role $role)
+    {
+        try {
+            DB::beginTransaction();
 
-          if ($role->users->count() >= 1) {
-               return response()->json([
+            if ($role->users->count() >= 1) {
+                return response()->json([
                     'message' => "Can't delete this data."
-               ], 400);
-          }
+                ], 400);
+            }
 
-          $softDeleted = $role->delete();
+            $softDeleted = $role->delete();
 
-          if ($softDeleted) {
-               $roleNameUpdated = $role->name = $role->name . '|' . $role->deleted_at;
-               $role->save();
-          }
+            if ($softDeleted) {
+                $roleNameUpdated = $role->name = $role->name . '|' . $role->deleted_at;
+                $role->save();
+            }
 
-          DB::commit();
-     } catch (\Throwable $th) {
-          DB::rollBack();
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
 
-          return response()->json([
-               'message' => 'Something went wrong, ' . $th->getMessage()
-          ], 500);
-     }
+            return response()->json([
+                'message' => 'Something went wrong, ' . $th->getMessage()
+            ], 500);
+        }
 
-     return response()->json([
-          'message' => 'Role successfully deleted.'
-     ], 201);
-   }
+        return response()->json([
+            'message' => 'Role successfully deleted.'
+        ], 201);
+    }
 }

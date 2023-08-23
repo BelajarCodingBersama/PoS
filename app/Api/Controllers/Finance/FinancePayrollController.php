961719cc8048b5bb72e1fb408ff05d9636759285
Vicky <Wibivicky@gmail.com>
2023-08-23 21:29:27 +0700

    update finance payroll feature

--- app/Api/Controllers/Finance/FinancePayrollController.php
+++ app/Api/Controllers/Finance/FinancePayrollController.php
@@ -2,12 +2,16 @@
 
 namespace App\Api\Controllers\Finance;
 
+use App\Api\Requests\PayrollStoreRequest;
 use App\Api\Requests\PayrollUpdateRequest;
 use App\Api\Resources\PayrollResource;
 use App\Api\Resources\PayrollResourceCollection;
 use App\Http\Controllers\Controller;
 use App\Models\Payroll;
+use App\Models\PayrollSetting;
+use App\Models\User;
 use App\Repositories\PayrollRepository;
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -23,10 +27,80 @@ class FinancePayrollController extends Controller
     public function index(Request $request)
     {
         $payrolls = $this->payrollRepository->get([
+            'search' => [
+                'name' => $request->name,
+                'month' => $request->month,
+                'year' => $request->year,
+                'status' => $request->status
+            ],
             'paginate' => $request->per_page
         ]);
 
         return new PayrollResourceCollection($payrolls);
+    }
+
+    public function store(PayrollStoreRequest $request)
+    {
+        $user = User::where('username', $request->name)->first();
+
+        $allowances = PayrollSetting::where('name', 'allowances')->first();
+        $allowancesType = $allowances->unitType->name;
+        
+        $tax = PayrollSetting::where('name', 'tax')->first();
+        $taxType = $tax->unitType->name;
+
+        try {
+            DB::beginTransaction();
+
+            $salary = $user->role->salary->nominal;
+            $nominalAllowance = 0;
+            $nominalTax = 0;
+            $paymentDate = $request->payment_date;
+            $paymentStatus = Payroll::STATUS_PENDING;
+            
+            if ($allowancesType == 'percent') {
+                $nominalAllowance = ($salary * $allowances->nominal) / 100;
+            } else if ($allowancesType == 'number') {
+                $nominalAllowance = $allowances->nominal;
+            }
+
+            if ($taxType == 'percent') {
+                $nominalTax = (($salary + $nominalAllowance) * $tax->nominal) / 100;
+            } else if ($taxType == 'number') {
+                $nominalTax = $tax->nominal;
+            }
+
+            if (!empty($paymentDate)) {
+                $paymentStatus = $request->status;
+            }
+
+            $payroll = new Payroll();
+
+            $data = [
+                'role' => $user->role->name,
+                'basic_salary' => $salary,
+                'allowances' => $nominalAllowance,
+                'tax' => $nominalTax,
+                'net_pay' => ($salary + $nominalAllowance) - $nominalTax,
+                'payment_date' => $paymentDate,
+                'status' => $paymentStatus,
+                'user_id' => $user->id
+            ];
+
+            $this->payrollRepository->save($payroll->fill($data));
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            return response()->json([
+                'message' => 'Something went wrong, ' . $th->getMessage()
+            ], 500);
+        }
+
+        return response()->json([
+            'message' => 'Payroll successfully created.'
+        ], 201);
     }
 
     public function show(Payroll $payroll)
@@ -36,14 +110,22 @@ class FinancePayrollController extends Controller
 
     public function update(PayrollUpdateRequest $request, Payroll $payroll)
     {
-        $this->authorize('update', $payroll);
-
         try {
             DB::beginTransaction();
 
-            $request->merge(['status' => Payroll::STATUS_PAID]);
+            $paymentDate = $request->payment_date;
 
-            $data = $request->only(['payment_date', 'status']);
+            if (!empty($paymentDate)) {
+                $paymentStatus = $request->status;
+            } 
+
+            $request->merge([
+                'status' => $paymentStatus
+            ]);
+
+            $data = $request->only([
+                'payment_date', 'status'
+            ]);
 
             $this->payrollRepository->save($payroll->fill($data));
 
@@ -58,6 +140,6 @@ class FinancePayrollController extends Controller
 
         return response()->json([
             'message' => 'Payroll successfully updated.'
-        ], 201);
+        ], 200);
     }
 }

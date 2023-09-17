2bfe1993e4ed064f3c9294f0ba0f925385c4b072
Dimas Pondra Oktafianto <77646429+DimasPondra@users.noreply.github.com>
2023-09-17 10:09:22 +0700

    :zap: add params

--- app/Api/Resources/CartResource.php
+++ app/Api/Resources/CartResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Api\Resources;
 
+use App\Helpers\RequestHelper;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -12,6 +13,11 @@ class CartResource extends JsonResource
         return [
             'id' => $this->id,
             'amount' => $this->amount,
+
+            'product' => $this->when(
+                RequestHelper::doesQueryParamsHasValue($request->query('include'), 'product'),
+                (new ProductResource($this->product))
+            )
         ];
     }
 }

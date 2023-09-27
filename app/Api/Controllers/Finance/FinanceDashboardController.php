74743a69cc46a4a056c0a63d3cdb3a24cc657c2e
Dimas Pondra Oktafianto <77646429+DimasPondra@users.noreply.github.com>
2023-09-27 15:19:38 +0700

    :zap: update response data dashboard

--- app/Api/Controllers/Finance/FinanceDashboardController.php
+++ app/Api/Controllers/Finance/FinanceDashboardController.php
@@ -4,73 +4,113 @@ namespace App\Api\Controllers\Finance;
 
 use App\Api\Resources\DashboardFinanceResource;
 use App\Http\Controllers\Controller;
+use App\Models\Payroll;
+use App\Models\Purchase;
+use App\Models\Transaction;
+use App\Models\TransactionDetail;
 use App\Repositories\PayrollRepository;
 use App\Repositories\PurchaseRepository;
+use App\Repositories\TransactionDetailRepository;
 use App\Repositories\TransactionRepository;
 use Illuminate\Http\Request;
 
 class FinanceDashboardController extends Controller
 {
-    private $purchaseRepository, $payrollRepository, $transactionRepository;
+    private $purchaseRepository, $payrollRepository, $transactionRepository,
+            $transactionDetailRepository;
 
     public function __construct(
         PurchaseRepository $purchaseRepository,
         PayrollRepository $payrollRepository,
-        TransactionRepository $transactionRepository
+        TransactionRepository $transactionRepository,
+        TransactionDetailRepository $transactionDetailRepository
     ) {
         $this->purchaseRepository = $purchaseRepository;
         $this->payrollRepository = $payrollRepository;
         $this->transactionRepository = $transactionRepository;
+        $this->transactionDetailRepository = $transactionDetailRepository;
     }
 
     public function index(Request $request)
     {
-        /** Purchase */
-        $purchases = $this->purchaseRepository->get();
-        $totalPricePurchases = 0;
-
-        foreach ($purchases as $purchase) {
-            $totalPricePurchases += $purchase->price;
-        }
-
-        /** Payroll */
-        $payrolls = $this->payrollRepository->get();
-        $totalPricePayrolls = 0;
-
-        foreach ($payrolls as $payroll) {
-            $totalPricePayrolls += $payroll->net_pay;
-        }
-
-        /** Total Expense */
-        $totalExpenses = $totalPricePurchases + $totalPricePayrolls;
-
-        /** Transaction & Total Income */
-        $transactions = $this->transactionRepository->get();
-        $totalIncomes = 0;
-
-        foreach ($transactions as $trx) {
-            $totalIncomes += $trx->total;
-        }
-
-        $data = [
-            'purchases' => [
-                'items' => $purchases,
-                'total' => $totalPricePurchases
+        /** Graph Purchase */
+        $purchases = $this->purchaseRepository->get([
+            'select' => 'YEAR(date) as year, MONTH(date) as month, SUM(price) as total, COUNT(*) as item',
+            'search' => [
+                'year' => $request->year
             ],
-            'payrolls' => [
-                'items' => $payrolls,
-                'total' => $totalPricePayrolls
+            'group' => 'year, month',
+            'order' => 'year ASC, month ASC'
+        ]);
+
+        /** Graph Payroll */
+        $payrolls = $this->payrollRepository->get([
+            'select' => 'YEAR(created_at) as year, MONTH(created_at) as month, SUM(net_pay) as total, COUNT(*) as item',
+            'search' => [
+                'year' => $request->year
             ],
-            'transactions' => [
-                'items' => $transactions,
-                'total' => $totalIncomes
+            'group' => 'year, month',
+            'order' => 'year ASC, month ASC'
+        ]);
+
+        /** Graph Transaction */
+        $transactions = $this->transactionRepository->get([
+            'select' => 'YEAR(created_at) as year, MONTH(created_at) as month, SUM(total) as total, COUNT(*) as item',
+            'search' => [
+                'year' => $request->year
             ],
-            'total_expenses' => $totalExpenses,
-            'total_incomes' => $totalIncomes
+            'group' => 'year, month',
+            'order' => 'year ASC, month ASC',
+        ]);
+
+        /** Total Purchase, Payroll, And Transaction */
+        $totalPurchase = $this->purchaseRepository->get([
+            'search' => [
+                'year' => $request->year
+            ],
+            'sum' => 'price'
+        ]);
+
+        $totalPayroll = $this->payrollRepository->get([
+            'search' => [
+                'year' => $request->year
+            ],
+            'sum' => 'net_pay'
+        ]);
+
+        $totalTransaction = $this->transactionRepository->get([
+            'search' => [
+                'year' => $request->year
+            ],
+            'sum' => 'total'
+        ]);
+
+        /** Popular Product */
+        $popular  = TransactionDetail::join('products', 'product_id', '=', 'products.id')
+                        ->selectRaw('products.name as product_name, SUM(transaction_details.amount) as total')
+                        ->when(!empty($request->year), function ($query) use ($request) {
+                            return $query->whereYear('transaction_details.created_at', $request->year);
+                        })
+                        ->groupByRaw('product_name')
+                        ->orderByRaw('total DESC')
+                        ->get();
+
+        $response = [
+            'data' => [
+                'graph' => [
+                    'purchase' => $purchases,
+                    'payroll' => $payrolls,
+                    'transaction' => $transactions
+                ],
+                'total' => [
+                    'purchase' => $totalPurchase,
+                    'payroll' => $totalPayroll,
+                    'transaction' => $totalTransaction
+                ],
+                'popular_products' => $popular
+            ]
         ];
 
-        return response()->json([
-            'data' => $data
-        ]);
+        return response()->json($response);
     }
 }
--- app/Repositories/PurchaseRepository.php
+++ app/Repositories/PurchaseRepository.php
@@ -15,7 +15,23 @@ class PurchaseRepository
 
     public function get($params = [])
     {
-        $purchases = $this->model;
+        $purchases = $this->model
+            ->when(!empty($params['select']), function ($query) use ($params) {
+                return $query->selectRaw($params['select']);
+            })
+            ->when(!empty($params['search']['year']), function ($query) use ($params) {
+                return $query->whereYear('created_at', $params['search']['year']);
+            })
+            ->when(!empty($params['group']), function ($query) use ($params) {
+                return $query->groupByRaw($params['group']);
+            })
+            ->when(!empty($params['order']), function ($query) use ($params) {
+                return $query->orderByRaw($params['order']);
+            });
+
+        if (!empty($params['sum'])) {
+            return $purchases->sum($params['sum']);
+        }
 
         if (!empty($params['paginate'])) {
             return $purchases->paginate($params['paginate']);

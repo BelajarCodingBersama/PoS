da11022222f7c807d53e7811025be89678b1b0c1
Dimas Pondra Oktafianto <77646429+DimasPondra@users.noreply.github.com>
2023-09-30 15:50:12 +0700

    :bug: bug fixing

--- app/Api/Controllers/Finance/FinancePurchaseController.php
+++ app/Api/Controllers/Finance/FinancePurchaseController.php
@@ -7,18 +7,23 @@ use App\Api\Requests\PurchaseUpdateRequest;
 use App\Api\Resources\PurchaseResource;
 use App\Api\Resources\PurchaseResourceCollection;
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use App\Models\Purchase;
+use App\Repositories\ProductRepository;
 use App\Repositories\PurchaseRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class FinancePurchaseController extends Controller
 {
-    private $purchaseRepository;
+    private $purchaseRepository, $productRepository;
 
-    public function __construct(PurchaseRepository $purchaseRepository)
-    {
+    public function __construct(
+        PurchaseRepository $purchaseRepository,
+        ProductRepository $productRepository
+    ) {
         $this->purchaseRepository = $purchaseRepository;
+        $this->productRepository = $productRepository;
     }
 
     public function index(Request $request)
@@ -35,6 +40,8 @@ class FinancePurchaseController extends Controller
         try {
             DB::beginTransaction();
 
+            $product = Product::findOrFail($request->product_id);
+
             $request->merge(['user_id' => auth()->id()]);
 
             $data = $request->only([
@@ -44,6 +51,9 @@ class FinancePurchaseController extends Controller
 
             $purchase = new Purchase();
             $this->purchaseRepository->save($purchase->fill($data));
+
+            $product['amount'] += $request->amount;
+            $this->productRepository->save($product);
 
             DB::commit();
         } catch (\Throwable $th) {
@@ -55,7 +65,7 @@ class FinancePurchaseController extends Controller
         }
 
         return response()->json([
-            'message' => 'Purchase successfully added.'
+            'message' => 'Purchase successfully created.'
         ], 201);
     }
 
@@ -75,6 +85,33 @@ class FinancePurchaseController extends Controller
                 'date', 'amount', 'price', 'product_id',
                 'seller_id'
             ]);
+
+            // Product ID hasn't changed
+            if ($purchase->product_id == $request->product_id) {
+                $product = Product::findOrFail($purchase->product_id);
+
+                // Amount has changed
+                if ($purchase->amount > $request->amount) {
+                    // Minus from before value
+                    $product['amount'] -= $purchase->amount - $request->amount;
+                } else if ($purchase->amount < $request->amount) {
+                    // Plus from before value
+                    $product['amount'] += $request->amount - $purchase->amount;
+                }
+
+                $this->productRepository->save($product);
+            } else {
+                // Product ID has changed
+                // Old product amount is returned to its original state
+                $oldProduct = Product::findOrFail($purchase->product_id);
+                $oldProduct['amount'] -= $purchase->amount;
+                $this->productRepository->save($oldProduct);
+
+                // Amount of new product added
+                $newProduct = Product::findOrFail($request->product_id);
+                $newProduct['amount'] += $request->amount;
+                $this->productRepository->save($newProduct);
+            }
 
             $this->purchaseRepository->save($purchase->fill($data));
 
@@ -98,6 +135,11 @@ class FinancePurchaseController extends Controller
 
         try {
             DB::beginTransaction();
+
+            // Product amount is returned to its original state
+            $product = Product::findOrFail($purchase->product_id);
+            $product['amount'] -= $purchase->amount;
+            $this->productRepository->save($product);
 
             $purchase->delete();
 

c1fb5c8087480e233ff6a40522b3de5481c1b419
Dimas Pondra Oktafianto <77646429+DimasPondra@users.noreply.github.com>
2023-11-01 17:22:18 +0700

    :hammer: refactor route

--- routes/api.php
+++ routes/api.php
@@ -33,155 +33,139 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('admin')->middleware('auth:sanctum', 'ability:admin,finance,cashier')->group(function () {
-    Route::prefix('products')->group(function () {
-        Route::get('/', [AdminProductController::class, 'index']);
-    });
+    Route::get('products', [AdminProductController::class, 'index']);
 });
 
 Route::prefix('admin')->middleware('auth:sanctum', 'ability:admin,finance')->group(function () {
-    Route::prefix('sellers')->group(function () {
-        Route::get('/', [AdminSellerController::class, 'index']);
-    });
-
-    Route::prefix('users')->group(function () {
-        Route::get('/', [AdminUserController::class, 'index']);
-    });
+    Route::get('sellers', [AdminSellerController::class, 'index']);
+    Route::get('users', [AdminUserController::class, 'index']);
 });
 
 Route::prefix('admin')->middleware('auth:sanctum', 'ability:admin,cashier')->group(function () {
-    Route::prefix('product-types')->group(function () {
-        Route::get('/', [AdminProductTypeController::class, 'index']);
-    });
-
-    Route::prefix('products')->group(function () {
-        Route::get('{product}/show', [AdminProductController::class, 'show']);
-    });
+    Route::get('product-types', [AdminProductTypeController::class, 'index']);
+    Route::get('products/{product}/show', [AdminProductController::class, 'show']);
 });
 
 Route::prefix('admin')->middleware('auth:sanctum', 'abilities:admin')->group(function () {
     Route::get('dashboard', [AdminDashboardController::class, 'index']);
 
-    Route::prefix('product-types')->group(function () {
-        Route::post('store', [AdminProductTypeController::class, 'store']);
-        Route::get('{productType}/show', [AdminProductTypeController::class, 'show']);
-        Route::patch('{productType}/update', [AdminProductTypeController::class, 'update']);
-        Route::delete('{productType}/delete', [AdminProductTypeController::class, 'destroy']);
+    Route::prefix('product-types')->controller(AdminProductTypeController::class)->group(function () {
+        Route::post('store', 'store');
+        Route::get('{productType}/show', 'show');
+        Route::patch('{productType}/update', 'update');
+        Route::delete('{productType}/delete', 'destroy');
     });
 
-    Route::prefix('products')->group(function () {
-        Route::post('store', [AdminProductController::class, 'store']);
-        Route::patch('{product}/update', [AdminProductController::class, 'update']);
-        Route::delete('{product}/delete', [AdminProductController::class, 'destroy']);
+    Route::prefix('products')->controller(AdminProductController::class)->group(function () {
+        Route::post('store', 'store');
+        Route::patch('{product}/update', 'update');
+        Route::delete('{product}/delete', 'destroy');
     });
 
-    Route::prefix('users')->group(function () {
-        Route::post('store', [AdminUserController::class, 'store']);
-        Route::get('{user}/show', [AdminUserController::class, 'show']);
-        Route::patch('{user}/update', [AdminUserController::class, 'update']);
+    Route::prefix('users')->controller(AdminUserController::class)->group(function () {
+        Route::post('store', 'store');
+        Route::get('{user}/show', 'show');
+        Route::patch('{user}/update', 'update');
     });
 
-    Route::prefix('roles')->group(function () {
-        Route::get('/', [AdminRoleController::class, 'index']);
-        Route::post('store', [AdminRoleController::class, 'store']);
-        Route::get('{role}/show', [AdminRoleController::class, 'show']);
-        Route::patch('{role}/update', [AdminRoleController::class, 'update']);
-        Route::delete('{role}/delete', [AdminRoleController::class, 'destroy']);
+    Route::prefix('roles')->controller(AdminRoleController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('store', 'store');
+        Route::get('{role}/show', 'show');
+        Route::patch('{role}/update', 'update');
+        Route::delete('{role}/delete', 'destroy');
     });
 
-    Route::prefix('files')->group(function () {
-        Route::post('store', [AdminFileController::class, 'store']);
+    Route::post('files/store', [AdminFileController::class, 'store']);
+
+    Route::prefix('transactions')->controller(AdminTransactionController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::get('{transaction}/show', 'show');
+        Route::get('{transaction}/export', 'export');
     });
 
-    Route::prefix('transactions')->group(function () {
-        Route::get('/', [AdminTransactionController::class, 'index']);
-        Route::get('{transaction}/show', [AdminTransactionController::class, 'show']);
-        Route::get('{transaction}/export', [AdminTransactionController::class, 'export']);
+    Route::prefix('sellers')->controller(AdminSellerController::class)->group(function () {
+        Route::post('store', 'store');
+        Route::get('{seller}/show', 'show');
+        Route::patch('{seller}/update', 'update');
+        Route::delete('{seller}/delete', 'destroy');
     });
 
-    Route::prefix('sellers')->group(function () {
-        Route::post('store', [AdminSellerController::class, 'store']);
-        Route::get('{seller}/show', [AdminSellerController::class, 'show']);
-        Route::patch('{seller}/update', [AdminSellerController::class, 'update']);
-        Route::delete('{seller}/delete', [AdminSellerController::class, 'destroy']);
+    Route::prefix('salaries')->controller(AdminSalaryController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('store', 'store');
+        Route::get('{salary}/show', 'show');
+        Route::patch('{salary}/update', 'update');
+        Route::delete('{salary}/delete', 'destroy');
     });
 
-    Route::prefix('salaries')->group(function () {
-        Route::get('/', [AdminSalaryController::class, 'index']);
-        Route::post('store', [AdminSalaryController::class, 'store']);
-        Route::get('{salary}/show', [AdminSalaryController::class, 'show']);
-        Route::patch('{salary}/update', [AdminSalaryController::class, 'update']);
-        Route::delete('{salary}/delete', [AdminSalaryController::class, 'destroy']);
+    Route::prefix('expense-types')->controller(AdminExpenseTypeController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('store', 'store');
+        Route::get('{expenseType}/show', 'show');
+        Route::patch('{expenseType}/update', 'update');
+        Route::delete('{expenseType}/delete', 'destroy');
     });
 
-    Route::prefix('expense-types')->group(function () {
-        Route::get('/', [AdminExpenseTypeController::class, 'index']);
-        Route::post('store', [AdminExpenseTypeController::class, 'store']);
-        Route::get('{expenseType}/show', [AdminExpenseTypeController::class, 'show']);
-        Route::patch('{expenseType}/update', [AdminExpenseTypeController::class, 'update']);
-        Route::delete('{expenseType}/delete', [AdminExpenseTypeController::class, 'destroy']);
+    Route::prefix('unit-types')->controller(AdminUnitTypeController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('store', 'store');
+        Route::get('{unitType}/show', 'show');
+        Route::patch('{unitType}/update', 'update');
+        Route::delete('{unitType}/delete', 'destroy');
     });
 
-    Route::prefix('unit-types')->group(function () {
-        Route::get('/', [AdminUnitTypeController::class, 'index']);
-        Route::post('store', [AdminUnitTypeController::class, 'store']);
-        Route::get('{unitType}/show', [AdminUnitTypeController::class, 'show']);
-        Route::patch('{unitType}/update', [AdminUnitTypeController::class, 'update']);
-        Route::delete('{unitType}/delete', [AdminUnitTypeController::class, 'destroy']);
-    });
-
-    Route::prefix('payroll-settings')->group(function () {
-        Route::get('/', [AdminPayrollSettingController::class, 'index']);
-        Route::post('store', [AdminPayrollSettingController::class, 'store']);
-        Route::get('{payrollSetting}/show', [AdminPayrollSettingController::class, 'show']);
-        Route::patch('{payrollSetting}/update', [AdminPayrollSettingController::class, 'update']);
-        Route::delete('{payrollSetting}/delete', [AdminPayrollSettingController::class, 'destroy']);
+    Route::prefix('payroll-settings')->controller(AdminPayrollSettingController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('store', 'store');
+        Route::get('{payrollSetting}/show', 'show');
+        Route::patch('{payrollSetting}/update', 'update');
+        Route::delete('{payrollSetting}/delete', 'destroy');
     });
 });
 
-Route::prefix('auth')->group(function () {
-    Route::post('login', [AuthController::class, 'login']);
+Route::prefix('auth')->controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
 
     Route::middleware('auth:sanctum', 'ability:admin,cashier,finance')->group(function () {
-        Route::get('show', [AuthController::class, 'show']);
-        Route::patch('change-password', [AuthController::class, 'changePassword']);
-        Route::post('logout', [AuthController::class, 'logout']);
+        Route::get('show', 'show');
+        Route::patch('change-password', 'changePassword');
+        Route::post('logout', 'logout');
     });
 });
 
 Route::prefix('cashier')->middleware('auth:sanctum', 'abilities:cashier')->group(function () {
-    Route::prefix('carts')->group(function () {
-        Route::get('/', [CashierCartController::class, 'index']);
-        Route::post('store', [CashierCartController::class, 'store']);
-        Route::get('{cart}/show', [CashierCartController::class, 'show']);
-        Route::patch('{cart}/update', [CashierCartController::class, 'update']);
-        Route::delete('{cart}/delete', [CashierCartController::class, 'destroy']);
+    Route::prefix('carts')->controller(CashierCartController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('store', 'store');
+        Route::get('{cart}/show', 'show');
+        Route::patch('{cart}/update', 'update');
+        Route::delete('{cart}/delete', 'destroy');
     });
 
-    Route::prefix('transactions')->group(function() {
-        Route::post('store', [CashierTransactionController::class, 'store']);
-    });
+    Route::post('transactions/store', [CashierTransactionController::class, 'store']);
 });
 
 Route::prefix('finance')->middleware('auth:sanctum', 'abilities:finance')->group(function () {
     Route::get('dashboard', [FinanceDashboardController::class, 'index']);
 
-    Route::prefix('purchases')->group(function () {
-        Route::get('/', [FinancePurchaseController::class, 'index']);
-        Route::post('store', [FinancePurchaseController::class, 'store']);
-        Route::get('{purchase}/show', [FinancePurchaseController::class, 'show']);
-        Route::patch('{purchase}/update', [FinancePurchaseController::class, 'update']);
-        Route::delete('{purchase}/delete', [FinancePurchaseController::class, 'destroy']);
-        Route::post('import', [FinancePurchaseController::class, 'import']);
-        Route::get('download-template', [FinancePurchaseController::class, 'download']);
+    Route::prefix('purchases')->controller(FinancePurchaseController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('store', 'store');
+        Route::get('{purchase}/show', 'show');
+        Route::patch('{purchase}/update', 'update');
+        Route::delete('{purchase}/delete', 'destroy');
+        Route::post('import', 'import');
+        Route::get('download-template', 'download');
     });
 
-    Route::prefix('payrolls')->group(function () {
-        Route::get('/', [FinancePayrollController::class, 'index']);
-        Route::post('store', [FinancePayrollController::class, 'store']);
-        Route::get('{payroll}/show', [FinancePayrollController::class, 'show']);
-        Route::patch('{payroll}/update', [FinancePayrollController::class, 'update']);
-        Route::get('report', [FinancePayrollController::class, 'print']);
-        Route::post('import', [FinancePayrollController::class, 'import']);
-        Route::get('download-template', [FinancePayrollController::class, 'download']);
+    Route::prefix('payrolls')->controller(FinancePayrollController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('store', 'store');
+        Route::get('{payroll}/show', 'show');
+        Route::patch('{payroll}/update', 'update');
+        Route::get('report', 'print');
+        Route::post('import', 'import');
+        Route::get('download-template', 'download');
     });
 });
